9ba056c8268c660c1eee4ecf6f798c7a8c131f03
DavisVaughan <davis@rstudio.com>
2019-12-24 11:56:31 -0500

    Export `new_ym()`

--- R/ym.R
+++ R/ym.R
@@ -8,12 +8,17 @@ ym <- function(x = integer()) {
   new_ym(out)
 }
 
+# ------------------------------------------------------------------------------
+
 # x = Number of days since 1970-01-01
 # Only allowed values should be month starts
 
 # Internally stored as double for compat with most Date operations,
 # but `ym()` forces input to be integer first to avoid fractional days
 
+# Exported, but it should be assumed that no checks are made for validity!
+
+#' @export
 new_ym <- function(x = double()) {
   vec_assert(x, ptype = double())
 
