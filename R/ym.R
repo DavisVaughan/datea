8ea2fe781e7361953054af731cd2ea2545cc8a8b
DavisVaughan <davis@rstudio.com>
2019-12-24 23:50:49 -0500

    Start working on docs and tests

--- R/ym.R
+++ R/ym.R
@@ -9,15 +9,37 @@ ym <- function(x = integer()) {
 
 # ------------------------------------------------------------------------------
 
-# x = Number of days since 1970-01-01
-# Only allowed values should be month starts
-
-# Internally stored as double for compat with most Date operations,
-# but `ym()` forces input to be integer first to avoid fractional days
-
-# Exported, but it should be assumed that no checks are made for validity!
-
+#' Construct a new year month
+#'
+#' @description
+#' `new_ym()` is a fast constructor of ym objects, with minimal checking.
+#' It is intended that `x` is restricted to only values that correspond to
+#' the first day of the month, but importantly this is not checked here. For
+#' more robust creation of ym objects, see [ym()] or [as_ym()].
+#'
+#' @details
+#' `new_ym()` is suitable for use as a prototype for the ym class.
+#'
+#' Internally, a ym is stored as the number of days since `"1970-01-01"`, the
+#' same as the Date class. It is stored as a double rather than an integer,
+#' since most of the time this is what Date does as well.
+#'
+#' A ym inherits from `"Date"` as its highest superclass, but also inherits
+#' from the class `"vctrs_vctr"` to gain the well-thought-out vctrs methods.
+#' There are a number of benefits gained from inheriting from `"Date"`. One of
+#' which is that functions that dispatch on `"Date"` will work automatically,
+#' like `lubridate::year()`.
+#'
 #' @export
+#' @examples
+#' # Internally stored as the number of days since 1970-01-01
+#' new_ym(0)
+#' new_ym(31)
+#'
+#' # But this is not enforced with this low-level constructor,
+#' # so technically you can create ym objects with invalid entries
+#' new_ym(2)
+#' as.Date(new_ym(2))
 new_ym <- function(x = double()) {
   vec_assert(x, ptype = double())
 
@@ -29,10 +51,27 @@ new_ym <- function(x = double()) {
 
 # ------------------------------------------------------------------------------
 
+#' Is `x` a ym?
+#'
+#' Test if `x` is a ym object.
+#'
+#' @param x `[vector]`
+#'
+#'   An object.
+#'
+#' @return
+#' A logical of length 1. `TRUE` if `x` is a ym, otherwise `FALSE`.
+#'
 #' @export
+#' @examples
+#' is_ym(ym(1))
+#'
+#' is_ym(1)
 is_ym <- function(x) {
   inherits(x, "ym")
 }
+
+# ------------------------------------------------------------------------------
 
 # We could leave this to `is.numeric.Date()` but I'd rather not
 
--- tests/testthat/test-ym.R
+++ tests/testthat/test-ym.R
@@ -1,3 +1,39 @@
+# ------------------------------------------------------------------------------
+# helper
+
+
+
+# ------------------------------------------------------------------------------
+# new
+
+test_that("can construct a ym prototype", {
+  expect_length(new_ym(), 0L)
+})
+
+test_that("can construct new ym objects", {
+  expect_s3_class(new_ym(0), "ym")
+  expect_length(new_ym(c(0, 1)), 2)
+})
+
+test_that("can technically create invalid ym objects", {
+  x <- new_ym(c(2, 1.5))
+  expect_s3_class(x, "ym")
+  expect_equal(vec_data(x), c(2, 1.5))
+})
+
+# ------------------------------------------------------------------------------
+# is
+
+test_that("can recognize ym() objects", {
+  expect_true(is_ym(ym()))
+  expect_false(is_ym(1))
+  expect_false(is_ym(new_date()))
+})
+
 test_that("ym() objects are correctly not numeric", {
   expect_false(is.numeric(ym()))
+})
+
+test_that("ym() objects are Dates", {
+  expect_s3_class(ym(), "Date")
 })
