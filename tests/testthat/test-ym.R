f855e73134fc8c8c7d655558ec42609b8bca8de9
DavisVaughan <davis@rstudio.com>
2019-12-26 07:03:49 -0500

    Add `ym()` tests

--- tests/testthat/test-ym.R
+++ tests/testthat/test-ym.R
@@ -1,7 +1,44 @@
 # ------------------------------------------------------------------------------
 # helper
 
+test_that("can create an empty ym()", {
+  expect_equal(ym(), new_ym())
+})
 
+test_that("can create from just years", {
+  expect_equal(ym(2019), ym(2019, 01))
+})
+
+test_that("can create from just months", {
+  expect_equal(ym(month = 2), ym(0, 2))
+})
+
+test_that("can create from year and month", {
+  expect_equal(ym(1970, 02), new_ym(31))
+})
+
+test_that("is vectorized", {
+  expect_equal(ym(c(1970, 1970), c(02, 03)), new_ym(c(31, 59)))
+})
+
+test_that("year and month must be the same size (no recycling)", {
+  expect_error(ym(1, c(1, 2)), "must be equal to the length")
+})
+
+test_that("year and month must be integer ish", {
+  expect_error(ym(year = 1.5), class = "vctrs_error_cast_lossy")
+  expect_error(ym(month = 1.5), class = "vctrs_error_cast_lossy")
+})
+
+test_that("year must be between 0 and 9999", {
+  expect_error(ym(year = -1), "`0` and `9999`")
+  expect_error(ym(year = 10000), "`0` and `9999`")
+})
+
+test_that("month must be between 1 and 12", {
+  expect_error(ym(month = 0), "`1` and `12`")
+  expect_error(ym(month = 13), "`1` and `12`")
+})
 
 # ------------------------------------------------------------------------------
 # new
