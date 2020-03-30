680870904bcdd4fbcdfcd33585d643f4bb3c9138
DavisVaughan <davis@rstudio.com>
2020-03-30 08:19:53 -0400

    Remove unused helpers

--- R/utils.R
+++ R/utils.R
@@ -10,20 +10,6 @@ tzone <- function(x) {
   attr(x, "tzone")[[1]] %||% ""
 }
 
-vec_dim <- function(x) {
-  dim <- dim(x)
-
-  if (is.null(dim)) {
-    return(1L)
-  }
-
-  length(dim)
-}
-
-is_one_dim <- function(x) {
-  vec_dim(x) == 1L
-}
-
 class_collapse <- function(x) {
   paste0(class(x), collapse = "/")
 }
