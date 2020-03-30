e276a5b59a5506c732af3e1859a785a50cecc757
DavisVaughan <davis@rstudio.com>
2020-03-30 08:17:57 -0400

    Revamp and test `as_ym()`

--- R/ym-as.R
+++ R/ym-as.R
@@ -1,4 +1,16 @@
+# TODO:
+# as_ym.integer()
+# as_ym.double()
+# as_ym.character()
+
 #' Coerce to year month
+#'
+#' @description
+#' Coerce a vector to ym. Supports a range of types including:
+#'
+#' - Date
+#' - POSIXct
+#' - POSIXlt
 #'
 #' @param x `[vector]`
 #'
@@ -9,14 +21,22 @@
 #'   Not used.
 #'
 #' @export
+#' @examples
+#' # Extra information such as days, hours, or time zones are dropped
+#' as_ym(as.Date("2019-05-03"))
+#' as_ym(as.POSIXct("2019-03-04 01:01:01", tz = "America/New_York"))
 as_ym <- function(x, ...) {
+  if (!missing(...)) {
+    ellipsis::check_dots_empty()
+  }
+
   UseMethod("as_ym")
 }
 
 #' @export
 as_ym.default <- function(x, ...) {
   class <- class_collapse(x)
-  abort(paste0("Can't coerce a `", class, "` to a `ym`."))
+  abort(paste0("Can't convert a <", class, "> to a <ym>."))
 }
 
 #' @export
@@ -26,15 +46,106 @@ as_ym.ym <- function(x, ...) {
 
 #' @export
 as_ym.Date <- function(x, ...) {
-  vec_cast(x, new_ym())
+  force_to_ym_from_date(x)
+}
+
+force_to_ym_from_date <- function(x) {
+  out <- warp_distance(x, period = "month")
+  out <- as.integer(out)
+  out <- new_ym(out)
+  names(out) <- names(x)
+  out
 }
 
 #' @export
-as_ym.POSIXt <- function(x, ...) {
-  vec_cast(x, new_ym())
+as_ym.POSIXct <- function(x, ...) {
+  force_to_ym_from_posixct(x)
+}
+
+force_to_ym_from_posixct <- function(x) {
+  force_to_ym_from_posixt(x)
+}
+
+force_to_ym_from_posixt <- function(x) {
+  # Drop time zone to avoid any DST weirdness in the
+  # `warp_distance(period = "month")` call
+  out <- as.Date(x)
+  out <- force_to_ym_from_date(out)
+  out
 }
 
 #' @export
-as_ym.numeric <- function(x, ...) {
-  vec_cast(x, new_ym())
+as_ym.POSIXlt <- function(x, ...) {
+  force_to_ym_from_posixlt(x)
+}
+
+force_to_ym_from_posixlt <- function(x) {
+  out <- force_to_ym_from_posixt(x)
+
+  # `as.Date.POSIXlt()` used in `force_to_ym_from_posixt()`
+  # doesn't retain names! Bug!
+  names(out) <- names(x)
+
+  out
+}
+
+# ------------------------------------------------------------------------------
+
+#' @export
+as.Date.ym <- function(x, ...) {
+  if (!missing(...)) {
+    ellipsis::check_dots_empty()
+  }
+
+  force_to_date_from_ym(x)
+}
+
+force_to_date_from_ym <- function(x) {
+  out <- months_to_days(x)
+  out <- new_date(out)
+  names(out) <- names(x)
+  out
+}
+
+# ------------------------------------------------------------------------------
+
+#' @export
+as.POSIXct.ym <- function(x, tz = "UTC", ...) {
+  if (!missing(...)) {
+    ellipsis::check_dots_empty()
+  }
+
+  force_to_posixct_from_ym(x, tz)
+}
+
+force_to_posixct_from_ym <- function(x, tz) {
+  force_to_posixt_from_ym(x, tz, posixct = TRUE)
+}
+
+force_to_posixt_from_ym <- function(x, tz, posixct) {
+  out <- force_to_date_from_ym(x)
+  out <- as.character(out)
+
+  if (posixct) {
+    out <- as.POSIXct(out, tz = tz)
+  } else {
+    out <- as.POSIXlt(out, tz = tz)
+  }
+
+  out
+}
+
+# ------------------------------------------------------------------------------
+
+#' @export
+as.POSIXlt.ym <- function(x, tz = "UTC", ...) {
+  if (!missing(...)) {
+    ellipsis::check_dots_empty()
+  }
+
+  force_to_posixlt_from_ym(x, tz)
+}
+
+force_to_posixlt_from_ym <- function(x, tz) {
+  force_to_posixt_from_ym(x, tz, posixct = FALSE)
 }
---
+++ tests/testthat/test-ym-as.R
@@ -0,0 +1,136 @@
+
+# ------------------------------------------------------------------------------
+# ym -> ym
+
+test_that("can force", {
+  expect_identical(as_ym(new_ym()), new_ym())
+})
+
+test_that("retains names", {
+  expect_named(as_ym(set_names(new_ym(0L), "x")), "x")
+})
+
+# ------------------------------------------------------------------------------
+# Date -> ym
+
+test_that("can force", {
+  expect_identical(as_ym(new_date(0)), new_ym(0L))
+})
+
+test_that("retains names", {
+  expect_named(as_ym(set_names(new_date(0), "x")), "x")
+})
+
+test_that("force is allowed to be lossy", {
+  expect_identical(as_ym(new_date(1)), new_ym(0L))
+})
+
+# ------------------------------------------------------------------------------
+# POSIXct -> ym
+
+test_that("can force", {
+  expect_identical(as_ym(new_datetime(0, "UTC")), new_ym(0L))
+})
+
+test_that("retains names", {
+  expect_named(as_ym(set_names(new_datetime(0), "x")), "x")
+})
+
+test_that("force is allowed to be lossy", {
+  expect_identical(as_ym(new_datetime(1, "UTC")), new_ym(0L))
+})
+
+test_that("time zone doesn't matter", {
+  expect_identical(
+    as_ym(new_datetime(0, "UTC")),
+    as_ym(new_datetime(0, "America/New_York"))
+  )
+})
+
+# ------------------------------------------------------------------------------
+# POSIXlt -> ym
+
+test_that("can force", {
+  datetime <- as.POSIXlt("1970-01-01", tz = "UTC")
+  expect_identical(as_ym(datetime), new_ym(0L))
+})
+
+test_that("retains names", {
+  datetime <- as.POSIXlt("1970-01-01", tz = "UTC")
+  expect_named(as_ym(set_names(datetime, "x")), "x")
+})
+
+test_that("force is allowed to be lossy", {
+  datetime <- as.POSIXlt("1970-01-01 01:01:01", tz = "UTC")
+  expect_identical(as_ym(datetime), new_ym(0L))
+})
+
+test_that("time zone doesn't matter", {
+  expect_identical(
+    as_ym(as.POSIXlt("1970-01-01", tz = "UTC")),
+    as_ym(as.POSIXlt("1970-01-01", tz = "America/New_York"))
+  )
+})
+
+# ------------------------------------------------------------------------------
+# ym -> Date
+
+test_that("can force", {
+  expect_identical(as.Date(new_ym(0L)), new_date(0))
+  expect_identical(as.Date(new_ym(1L)), new_date(31))
+})
+
+test_that("retains names", {
+  expect_named(as.Date(set_names(new_ym(0L), "x")), "x")
+})
+
+# ------------------------------------------------------------------------------
+# ym -> POSIXct
+
+test_that("can force", {
+  expect_identical(as.POSIXct(new_ym(0L)), as.POSIXct("1970-01-01", tz = "UTC"))
+  expect_identical(as.POSIXct(new_ym(1L)), as.POSIXct("1970-02-01", tz = "UTC"))
+})
+
+test_that("retains names", {
+  expect_named(as.POSIXct(set_names(new_ym(0L), "x")), "x")
+})
+
+test_that("default tz is UTC", {
+  expect_identical(attr(as.POSIXct(new_ym(0L)), "tzone", exact = TRUE), "UTC")
+})
+
+test_that("can modify time zone and keep clock time", {
+  expect_identical(as.POSIXct(new_ym(0L), tz = "America/New_York"), as.POSIXct("1970-01-01", tz = "America/New_York"))
+  expect_identical(as.POSIXct(new_ym(1L), tz = "America/New_York"), as.POSIXct("1970-02-01", tz = "America/New_York"))
+})
+
+# ------------------------------------------------------------------------------
+# ym -> POSIXlt
+
+test_that("can force", {
+  expect_identical(as.POSIXlt(new_ym(0L)), as.POSIXlt("1970-01-01", tz = "UTC"))
+  expect_identical(as.POSIXlt(new_ym(1L)), as.POSIXlt("1970-02-01", tz = "UTC"))
+})
+
+test_that("retains names", {
+  expect_named(as.POSIXlt(set_names(new_ym(0L), "x")), "x")
+})
+
+test_that("default tz is UTC", {
+  expect_identical(attr(as.POSIXlt(new_ym(0L)), "tzone", exact = TRUE), "UTC")
+})
+
+test_that("can modify time zone and keep clock time", {
+  expect_identical(as.POSIXlt(new_ym(0L), tz = "America/New_York"), as.POSIXlt("1970-01-01", tz = "America/New_York"))
+  expect_identical(as.POSIXlt(new_ym(1L), tz = "America/New_York"), as.POSIXlt("1970-02-01", tz = "America/New_York"))
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("`as_ym()` gives informative errors", {
+  verify_output(test_path("errors", "test-ym-as.txt"), {
+    "# default method collapses classes"
+    as_ym(ordered(factor("x")))
+  })
+})
