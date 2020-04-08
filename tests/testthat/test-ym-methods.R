d7d6ae77f40f34506ae05a25edfa631fdac8cfb8
DavisVaughan <davis@rstudio.com>
2020-04-08 17:50:46 -0400

    Fix year/month types

--- tests/testthat/test-ym-methods.R
+++ tests/testthat/test-ym-methods.R
@@ -8,8 +8,8 @@ test_that("lubridate::tz() returns UTC", {
 test_that("lubridate accessors work", {
   x <- new_ym(0L)
 
-  expect_identical(lubridate::year(x), 1970L)
-  expect_identical(lubridate::month(x), 1L)
+  expect_identical(lubridate::year(x), 1970)
+  expect_identical(lubridate::month(x), 1)
   expect_identical(lubridate::day(x), 1L)
   expect_identical(lubridate::hour(x), 0L)
   expect_identical(lubridate::minute(x), 0L)
