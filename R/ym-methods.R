4a08a2ad7de3d147728e697fb41b8eae2e7e671f
DavisVaughan <davis@rstudio.com>
2020-04-09 10:06:09 -0400

    Add `[` method

--- R/ym-methods.R
+++ R/ym-methods.R
@@ -3,3 +3,15 @@
 tz.ym <- function(x) {
   "UTC"
 }
+
+# Only implemented to have better handling of extra indices passed in `...`
+#' @export
+`[.ym` <- function(x, i, ...) {
+  if (!missing(...)) {
+    ellipsis::check_dots_empty()
+  }
+
+  i <- maybe_missing(i, TRUE)
+
+  vec_slice(x, i)
+}
--- tests/testthat/test-ym-methods.R
+++ tests/testthat/test-ym-methods.R
@@ -15,3 +15,41 @@ test_that("lubridate accessors work", {
   expect_identical(lubridate::minute(x), 0L)
   expect_identical(lubridate::second(x), 0)
 })
+
+# ------------------------------------------------------------------------------
+# vctrs backed methods
+
+test_that("as.list() works", {
+  x <- set_names(new_ym(1:2), c("x", "y"))
+
+  expect <- list(
+    set_names(new_ym(1L), "x"),
+    set_names(new_ym(2L), "y")
+  )
+
+  expect_identical(as.list(x), expect)
+})
+
+test_that("c() works as best as it can", {
+  expect_identical(c(new_ym(1L), new_ym(2L)), new_ym(1:2))
+
+  # Nothing we can do about the second case
+  expect_error(c(new_ym(1L), 1), class = "vctrs_error_incompatible_type")
+  expect_identical(c(1, new_ym(1L)), c(1, 1))
+})
+
+test_that("vec_c() works correctly where c() fails", {
+  expect_error(vec_c(1, new_ym(1L)), class = "vctrs_error_incompatible_type")
+})
+
+test_that("`[` works", {
+  expect_identical(new_ym(1:2)[], new_ym(1:2))
+  expect_identical(new_ym(1:2)[1], new_ym(1L))
+
+  x <- set_names(new_ym(1:2), c("x", "y"))
+  expect_identical(x["y"], set_names(new_ym(2L), "y"))
+})
+
+test_that("`[` has nice error message with extra indices", {
+  expect_error(new_ym(1L)[1, 1], class = "rlib_error_dots_nonempty")
+})
