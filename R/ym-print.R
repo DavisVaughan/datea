405b2912a038637e9d0241a601a02c74d8105ee7
DavisVaughan <davis@rstudio.com>
2020-04-09 09:57:34 -0400

    Document format method rationale

--- R/ym-print.R
+++ R/ym-print.R
@@ -1,4 +1,23 @@
+#' Format a ym
+#'
+#' The ym `format()` method is purposefully implemented without a `format =`
+#' argument, and always prints as `YYYY-MM`. To use a custom format, the ym
+#' would have to be converted to POSIXlt, which is a very slow operation. It is
+#' also buggy when outside the year range of 0-9999 because strptime doesn't
+#' support it. If you want to use a custom format, convert to Date first with
+#' [as.Date()], and then format that.
+#'
+#' @param x A ym.
+#' @param ... Unused.
+#'
 #' @export
+#'
+#' @examples
+#' format(ym(2019, 01))
+#'
+#' # NA values are printed as the actual character string
+#' # for better behavior when combined with `print(format(x), quote = FALSE)`.
+#' format(ym(NA, NA))
 format.ym <- function(x, ...) {
   if (!missing(...)) {
     ellipsis::check_dots_empty()
